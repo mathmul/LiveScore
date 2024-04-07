fd288c42524ea2478f22bac9d1918a648acbe59e
mathmul <mmulej@gmail.com>
2024-04-07 03:25:20 +0200

    FIXME; Pass App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen, waitFor} from '@testing-library/react';
 import App from './App';
+import axios from "axios";
+import {LiveScoreResponse} from "./services/api";
 
-test('renders learn react link', () => {
+// FIXME: Not sure how to resolve issues with axios import other than adding all this extra code
+// FIXME: ## FROM
+jest.mock('axios', () => ({
+  __esModule: true,
+  default: {
+    get: jest.fn(),
+    isAxiosError: jest.fn()
+  }
+}));
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+// FIXME: ## TO
+
+test('renders learn LiveScore component', async () => {
+  // FIXME: ## FROM
+  const mockData: LiveScoreResponse = {
+    phase: 'match',
+    teams: [
+      { team_id: 1, team_name: 'Team A', team_name_short: 'TA' },
+      { team_id: 2, team_name: 'Team B', team_name_short: 'TB' },
+    ],
+    matches: [{ match_id: 1, tournament_id: 101, round: 1, home_team_id: 1, away_team_id: 2 }],
+    events: [
+      { event_id: 1, event_type: "match_start", event_time: 0, match_id: 1},
+      { event_id: 2, event_type: 'goal', event_time: 30, match_id: 1, score_amount: 1, score_team: 'home' }
+    ],
+  };
+  mockedAxios.get.mockReturnValue({ data: mockData } as any);
+  // FIXME: ## TO
+
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  await waitFor(() => expect(screen.getByTestId('LiveScore')).toBeInTheDocument());
 });
