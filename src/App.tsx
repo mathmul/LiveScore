f133571f3a94a6de6a8d62e7dbd694a2d039769d
mathmul <mmulej@gmail.com>
2024-04-07 02:41:14 +0200

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Tmp from './Tmp';
-import MatchRow, { MatchRowProps } from "./components/MatchRow/MatchRow";
-
+import LiveScore from "./components/LiveScore/LiveScore";
 
 function App() {
-    const props: MatchRowProps = {
-        home_team: {
-            abbreviation: "SVN",
-            flagSrc: "https://flagcdn.com/w40/si.png",
-            name: "Slovenia",
-        },
-        away_team: {
-            abbreviation: "ESP",
-            flagSrc: "https://flagcdn.com/w40/es.png",
-            name: "Spain",
-        },
-        homeScore: 1,
-        awayScore: 0,
-    }
-
-  return (
-    <div className="App">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <br/>
-            <MatchRow { ...props } />
-            <br/>
-            <br/>
-            <Tmp/>
-        </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <br/>
+                <LiveScore/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
